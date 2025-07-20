5b6eb9db1f3c300006c8e086063fa821b8586c4d
Donob <hxm0070@gmail.com>
2025-07-21 02:15:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,14 +6,22 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        path = self.path.split('?')[1]
-        # user = path.split('?')[1]
-        data = {
-            "path": path
-        }
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        path = self.path
+        if "?" in path:
+            img_name = path.split('?')[1]
+            data = {
+                "img_name": img_name
+            }
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        else:
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-type', 'text')
+            self.end_headers()
+            self.wfile.write("Hello braimg!".encode('utf-8'))
+        
         return
