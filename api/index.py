6b80f764d95e8559d48c82a588c469a63bef2887
Donob <hxm0070@gmail.com>
2025-07-21 03:09:27 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,13 +8,21 @@ GIT_REPO = environ.get("GIT_REPO")
 GIT_USER = environ.get("GIT_USER")
 GIT_TOKEN = environ.get("GIT_TOKEN")
 
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         path = self.path
         if "?" in path:
-            img_name = path.split('?')[1]
+            img_name = path.split('?')[1]            
+            git_url = "https://api.github.com/repos/{user}/{repo}/contents/{img_name}".format(user=GIT_USER, repo=GIT_REPO, img_name=img_name)
+            headers = {
+                "Accept": "application/vnd.github+json",
+                "Authorization": "Bearer {git_token}".format(git_token=GIT_TOKEN),
+                "X-GitHub-Api-Version": "2022-11-28",
+            }
+            res = requests.get(url=git_url, headers=headers)
             data = {
-                "url": "https://api.github.com/repos/{user}/{repo}/contents/{img_name}".format(user=GIT_USER, repo=GIT_REPO, img_name=img_name)
+                "url": res.json()
             }
             self.send_response(200)
             self.send_header('Access-Control-Allow-Origin', '*')
